7b5da00f657dc091499d9aa67d8b6b2dd7fbe59a
Teun van Schagen <551120@student.inholland.nl>
2018-10-30 12:43:31 +0100

    added heroku integration fix

--- index.js
+++ index.js
@@ -4,30 +4,24 @@ var bodyParser = require('body-parser')
 const axios = require('axios')
 var kenteken = require('./kenteken.js');
 
-app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.json())
+
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
-) // for parsing application/x-www-form-urlencoded
+)
 
-//This is the route the API will call
 app.post('/new-message', function(req, res) {
   const { message } = req.body
 
-  //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
-
-  // check of bericht niet leeg is
   if (!message) {
     return res.end()
   }
 
-  // check of kenteken in de regex valt van bekende kentekens
   if (!kenteken.check(message.text)) {
     return res.end();
   }
-
-  // get data
 
   var data;
 
@@ -36,18 +30,17 @@ app.post('/new-message', function(req, res) {
       'https://opendata.rdw.nl/resource/m9d7-ebf2.json?kenteken=' + message.text.replace(/-/g, '').toUpperCase()
     )
     .then(response => {
-      data = response.body
-      console.log('Received response from RDW: ')
-      console.log(response.data)
+      // console.log(response.data)
       data = response.data[0]
+      answer = data.merk + ' ' + data.handelsbenaming + ', ' + data.aantal_cilinders + ' cilinders, inhoud van ' + data.cilinderinhoud + 'cc'
+      console.log(answer)
 
       axios
       .post(
         'https://api.telegram.org/bot664166564:AAGyQI9Q7BBNDYr1oFJ-w4UTTh92GY3Q1mw/sendMessage',
         {
           chat_id: message.chat.id,
-          //text:  JSON.stringify(data)
-          text: data.merk + ' ' + data.handelsbenaming + ', ' + data.aantal_cilinders + ' cilinders, inhoud van ' + data.cilinderinhoud + 'cc'
+          text: answer
         }
       )
       .then(response => {
